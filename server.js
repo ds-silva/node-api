208490fbc5abf9336c065d6973096c2ca311c188
David Barbosa da Silva <davidinteraction@outlook.com>
2020-06-28 22:25:11 -0300

    Conectando Database - Criando model de produto

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
+
+// Iniciando o App
 const app = express();
 
-app.get('/', (req, resp) => {
-    resp.send("Hello Rocketseat by Ds");
+// Iniciando o DB
+mongoose.connect(
+    "mongodb://localhost:27017/nodeapi", 
+    { useNewUrlParser: true }
+);
+
+requireDir("./src/models");
+
+const Product = mongoose.model("Product");
+
+// Primeira rota
+app.get("/", (req, resp) => {
+
+    Product.create({
+        title: "React Native",
+        descripction: "Build native apps with React",
+        url: "http://github.com/facebook/react-native"
+    });
+
+    return resp.send("Server Ok");
 });
 
 app.listen(3001);
