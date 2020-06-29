5cd0af3133cf817d3a50caace75416e09cb29a62
David Barbosa da Silva <davidinteraction@outlook.com>
2020-06-28 22:53:30 -0300

    Criação de registro

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -3,8 +3,13 @@ const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
 module.exports = {
-    async index(rep, res) {
+    async index(req, res) {
         const products = await Product.find();
-        return resp.json(products)
+        return res.json(products);
+    },
+
+    async store(req, res) {
+        const product = await Product.create(req.body);
+        return res.json(product);
     },
 }
